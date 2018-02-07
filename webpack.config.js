516a580c7accbfc8c904b0671105c190cd4787a2
Uli Horn <ulihorn@gmail.com>
2018-02-07 16:39:06 +0200

    webpack.config.js module rule css

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,48 @@
 const path = require('path');
+//const webpack = require('webpack');
+const uglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
 module.exports = {
   entry: path.join(__dirname, 'src', 'js', 'app'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist'
   },
   module: {
-    rules: [{
-      test: /.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'src')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules'),
-        path.resolve(__dirname, 'bower_components')
-      ],
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015']
+    rules: [
+      {
+        test: /.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'bower_components')
+        ],
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /.css$/,
+        include: [
+          path.resolve(__dirname, 'src', 'css')
+        ],
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
-    }]
+    ]
   },
+  plugins: [
+    //new webpack.optimize.UglifyJsPlugin({
+    new uglifyJsPlugin({
+      sourceMap: true
+    })
+  ],
   resolve: {
     extensions: ['.json', '.js', '.jsx', '.css']
   },
