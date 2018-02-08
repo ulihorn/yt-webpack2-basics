1d001bfea54bbd24be61143fd92319e9a083da03
Uli Horn <ulihorn@gmail.com>
2018-02-08 14:24:47 +0200

    added multiple html files using HtmlWebPackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 //const webpack = require('webpack');
 const uglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
@@ -12,7 +14,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist'
+    //publicPath: '/dist'
   },
   module: {
     rules: [
@@ -43,10 +45,37 @@ module.exports = {
             'sass-loader'
           ]
         })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              //publicPath: 'img/'
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'users.html',
+      template: 'src/users.html',
+      chunks: []
+    }),
     //new webpack.optimize.UglifyJsPlugin({
     new uglifyJsPlugin({
       sourceMap: true
@@ -56,8 +85,8 @@ module.exports = {
   resolve: {
     extensions: ['.json', '.js', '.jsx', '.css']
   },
-  devtool: 'source-map',
-  devServer: {
-    publicPath: path.join('/dist/')
-  }
+  devtool: 'source-map'
+  //devServer: {
+  //  publicPath: path.join('./')
+  //}
 };
