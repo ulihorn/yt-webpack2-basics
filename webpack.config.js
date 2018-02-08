b9074fc3d96788630dd467b924b6e38129517a1d
Uli Horn <ulihorn@gmail.com>
2018-02-08 10:04:52 +0200

    moving html and image files into src

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 //const webpack = require('webpack');
 const uglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
+// var extractPlugin = new ExtractTextPlugin({
+//   filename: 'main.css'
+// });
+
 module.exports = {
-  entry: path.join(__dirname, 'src', 'js', 'app'),
+  entry: path.join(__dirname, 'src', 'js', 'app1'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -12,28 +17,32 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.jsx?$/,
+        test: /.js$/,
         include: [
-          path.resolve(__dirname, 'src')
+          path.resolve(__dirname, 'src', 'js')
         ],
         exclude: [
           path.resolve(__dirname, 'node_modules'),
           path.resolve(__dirname, 'bower_components')
         ],
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015']
+            }
+          }
+        ]
       },
       {
-        test: /.css$/,
-        include: [
-          path.resolve(__dirname, 'src', 'css')
-        ],
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            'css-loader',
+            'sass-loader'
+          ]
+        })
       }
     ]
   },
@@ -41,7 +50,8 @@ module.exports = {
     //new webpack.optimize.UglifyJsPlugin({
     new uglifyJsPlugin({
       sourceMap: true
-    })
+    }),
+    new ExtractTextPlugin("main.css")
   ],
   resolve: {
     extensions: ['.json', '.js', '.jsx', '.css']
